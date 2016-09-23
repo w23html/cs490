9e8a11b20cb0ade097386ce8cc2698b995ee07d1
sine27 <feng87@purdue.edu>
2016-09-23 19:13:14 -0400

    Shop setup
    set up the UI
    not yet enable onClick()

---
+++ FunTuma/Assets/script/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopController : MonoBehaviour {
+
+	public Canvas shopCanvas;
+
+	// Use this for initialization
+	void Start () 
+	{
+		openShop ();
+	}
+
+	void openShop()
+	{
+		shopCanvas.enabled = true;
+		Time.timeScale = 0;
+	}
+
+	public void closeShop()
+	{
+		shopCanvas.enabled = false;
+		Time.timeScale = 1;
+	}
+}
